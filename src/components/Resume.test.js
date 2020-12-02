9513061c882ca2965abc4f5217cb4fd8aa775d54
Taylor Morgan <taylor.morgan@gmail.com>
2020-12-02 08:26:50 -0500

    Note about msw not working on codesandbox

--- src/components/Resume.test.js
+++ src/components/Resume.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import WithProviders from "../WithProviders";
 import Resume from "./Resume";
-import people from "../mocks/people.json"
+import people from "../mocks/people.json";
 
 describe("loads and displays output", () => {
   beforeEach(() => {
@@ -13,32 +13,32 @@ describe("loads and displays output", () => {
       </WithProviders>
     );
   });
- 
+
   test("renders loading message", () => {
     screen.getByText(/Loading/i);
   });
 
-  test("renders display options", () => {
-    screen.findByTestId("display-options");
-  });
- 
-  test("renders overview",  () => {
-    screen.findByTestId("overview");
+  test("renders display options", async () => {
+    await screen.findByTestId("display-options");
   });
 
-  test("renders test person from mocked api",  () => {
-    screen.findByText(`${people[0].firstName} ${people[0].lastName}`);
+  test("renders overview", async () => {
+    await screen.findByTestId("overview");
   });
 
-  test("renders experience list", () => {
-    screen.findByText("Experience");
+  test("renders test person from mocked api (wont work on codesandbox)", async () => {
+    await screen.findByText(`${people[0].firstName} ${people[0].lastName}`);
   });
 
-  test("renders education list", () => {
-    screen.findByText("Education");
+  test("renders experience list", async () => {
+    await screen.findByText("Experience");
   });
 
-  test("renders certification list", () => {
-    screen.findByText("Certifications");
+  test("renders education list", async () => {
+    await screen.findByText("Education");
+  });
+
+  test("renders certification list", async () => {
+    await screen.findByText("Certifications");
   });
 });
