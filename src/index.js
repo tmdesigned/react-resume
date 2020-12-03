729c35d46de394514dbc5b4cfcedd1cfb137d195
Taylor Morgan <taylor@losant.com>
2020-12-03 07:20:35 -0500

    Add env flag for mock data

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // use test data on frontend (service worker, will not work in codesandbox)
-// const { worker } = require("./mocks/browser");
-// worker.start();
+if(process.env.REACT_APP_MOCK){
+ const { worker } = require("./mocks/browser");
+ worker.start();
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
